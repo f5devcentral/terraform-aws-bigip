742d4baea92a221c8df89b30d87216c970cb224e
Cody Green <cody@codygreen.com>
2019-09-26 16:11:08 -0500

    move varables from example main to variables file

--- examples/3_nic_with_new_vpc/main.tf
+++ examples/3_nic_with_new_vpc/main.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = local.region
 }
 
-# Variable for the EC2 Key 
-# Set via CLI or via terraform.tfvars file
-variable "ec2_key_name" {
-  description = "AWS EC2 Key name for SSH access"
-  type        = string
-}
-
 #
 # Create a random id
 #
