d34321d323a96403912b9ee4576243cb4dfd56ec
Mark Menger <m.menger@f5.com>
2020-05-08 14:42:16 +0000

    add comments to runtests.sh

--- modules/3-nic/examples/runtests.sh
+++ modules/3-nic/examples/runtests.sh
@@ -1,12 +1,12 @@
-#terraform show -json | jq '.values.root_module.resources[] | select(.address | contains("random_password.bigippassword"))  | .values.result' > inspec/bigip-ready/files/password.json
-
+# extract the BIG-IP addresses from the Terraform output
 export BIGIP_IPS=`terraform output --json | jq -cr '.bigip_mgmt_ips.value[]'`
 export BIGIP_USER=admin
+# extract the BIG-IP password from the Terraform state
 export BIGIP_PASSWORD=`terraform show -json | jq .values.root_module.resources[] | jq -r 'select(.address | contains("random_password")).values.result'`
 export DO_VERSION=1.11.1
 export AS3_VERSION=3.13.2
 export TS_VERSION=1.10.0
-
+# call the Automation Toolchain test profile
 for ip in $BIGIP_IPS; do 
     inspec exec bigip-atc --reporter cli --show-progress --input bigip_address=$ip bigip_port=443 user=$BIGIP_USER password=$BIGIP_PASSWORD do_version=$DO_VERSION as3_version=$AS3_VERSION ts_version=$TS_VERSION
 done
